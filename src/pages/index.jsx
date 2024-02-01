1f094412c01b9d884d9413b74524e9ece21a5043
sanjaykumbhar26 <webvedkumbhar@gmail.com>
2024-02-01 16:42:22 +0530

    fixed app layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,12 +16,29 @@ import {
 } from "@cloudscape-design/components";
 import { I18nProvider } from "@cloudscape-design/components/i18n";
 import messages from "@cloudscape-design/components/i18n/messages/all.en";
-
 import WebViewer from "@pdftron/webviewer";
 import ButtonDropdown from "@cloudscape-design/components/button-dropdown";
+import { applyTheme } from '@cloudscape-design/components/theming';
+import TopNavigation from "@cloudscape-design/components/top-navigation";
+
+
+const theme = {
+   tokens: {
+      colorBackgroundLayoutMain: {
+          light: '#f4f4f4',
+          dark: 'blue'
+      }
+   },
+};
+
+applyTheme({ theme });
 
 const headerStyle = {
-  padding: 0, // Set padding to 0 to remove it
+  padding: 0,
+};
+
+const appStyle = {
+  backgroundColor: '#000',
 };
 
 // Assign your function to a variable
@@ -47,54 +64,133 @@ export function Home() {
 
   return (
     <I18nProvider locale={LOCALE} messages={[messages]}>
+      <TopNavigation
+        id="topNavigation"
+      identity={{
+        href: "#",
+        title: "Service",
+        logo: {
+          src: "/RD_Horizozntal_White_Logo@2x.png",
+          alt: "Service"
+        }
+      }}
+      utilities={[
+        {
+          type: "button",
+          text: "Link",
+          href: "#",
+          external: true,
+          externalIconAriaLabel: " (opens in a new tab)"
+        },
+        {
+          type: "button",
+          iconName: "notification",
+          title: "Notifications",
+          ariaLabel: "Notifications (unread)",
+          badge: true,
+          disableUtilityCollapse: false
+        },
+        {
+          type: "menu-dropdown",
+          iconName: "settings",
+          ariaLabel: "Settings",
+          title: "Settings",
+          items: [
+            {
+              id: "settings-org",
+              text: "Organizational settings"
+            },
+            {
+              id: "settings-project",
+              text: "Project settings"
+            }
+          ]
+        },
+        {
+          type: "menu-dropdown",
+          text: "Customer Name",
+          description: "email@example.com",
+          iconName: "user-profile",
+          items: [
+            { id: "profile", text: "Profile" },
+            { id: "preferences", text: "Preferences" },
+            { id: "security", text: "Security" },
+            {
+              id: "support-group",
+              text: "Support",
+              items: [
+                {
+                  id: "documentation",
+                  text: "Documentation",
+                  href: "#",
+                  external: true,
+                  externalIconAriaLabel:
+                    " (opens in new tab)"
+                },
+                { id: "support", text: "Support" },
+                {
+                  id: "feedback",
+                  text: "Feedback",
+                  href: "#",
+                  external: true,
+                  externalIconAriaLabel:
+                    " (opens in new tab)"
+                }
+              ]
+            },
+            { id: "signout", text: "Sign out" }
+          ]
+        }
+      ]}
+    />
       <ContentLayout
         disableOverlap
         disableContentPaddings
-        header={
-          <Header
-            className="header "
-            variant="h1"
-            actions={
-              <SpaceBetween direction="horizontal" size="xs">
-                <ButtonDropdown
-                  items={[
-                    { text: "Delete", id: "rm", disabled: false },
-                    { text: "Move", id: "mv", disabled: false },
-                    { text: "Rename", id: "rn", disabled: true },
-                    {
-                      id: "view",
-                      text: "View metrics",
-                      href: "https://example.com",
-                      external: true,
-                      externalIconAriaLabel: "(opens in new tab)",
-                    },
-                  ]}
-                  className="buttonDropdown"
-                >
-                  Short
-                </ButtonDropdown>
-              </SpaceBetween>
-            }
-            disableContentPaddings
-            style={headerStyle}
-          >
-            <SpaceBetween direction="horizontal" size="xs">
-              <img
-                src="/RD_Horizozntal_White_Logo@2x.png"
-                alt="placeholder"
-                width="10%"
-              />
-              Adobe Acrobat Sign
-            </SpaceBetween>
-          </Header>
-        }
+        // header={
+        //   <Header
+        //     className="header"
+        //     disableOverlap
+        //     disableContentPaddings
+        //     actions={
+        //       <ButtonDropdown
+        //           items={[
+        //             { text: "Delete", id: "rm", disabled: false },
+        //             { text: "Move", id: "mv", disabled: false },
+        //             { text: "Rename", id: "rn", disabled: true },
+        //             {
+        //               id: "view",
+        //               text: "View metrics",
+        //               href: "https://example.com",
+        //               external: true,
+        //               externalIconAriaLabel: "(opens in new tab)",
+        //             },
+        //           ]}
+        //           className="buttonDropdown"
+        //         >
+        //           Short
+        //         </ButtonDropdown>
+        //     }
+        //     style={headerStyle}
+        //   >
+        //         <img
+        //           src="/RD_Horizozntal_White_Logo@2x.png"
+        //           alt="placeholder"
+        //           width="130px"
+        //         />
+        //         Adobe Acrobat Sign
+        //   </Header>
+        // }
       >
         <AppLayout
           contentType="default"
-          navigationOpen={navOpen}
-          onNavigationChange={(changeDetails) => {
-            setNavOpen(changeDetails.detail.open);
-          }}
+          navigationOpen={true}
+          style={appStyle}
+          headerSelector="#topNavigation"
+          className="appLayout"
+          // navigationOpen={navOpen}
+          // onNavigationChange={(changeDetails) => {
+          //   setNavOpen(changeDetails.detail.open);
+          // }}
           navigation={
             <SideNavigation
               header={{
@@ -104,20 +200,20 @@ export function Home() {
               items={[{ type: "link", text: `Paage #1`, href: `#` }]}
             />
           }
-          notifications={
-            <Box padding={{ top: "s" }}>
-              <Flashbar
-                items={[
-                  {
-                    type: "info",
-                    dismissible: true,
-                    content: "This is an info flash message.",
-                    id: "message_1",
-                  },
-                ]}
-              />
-            </Box>
-          }
+          // notifications={
+          //   <Box padding={{ top: "s" }}>
+          //     <Flashbar
+          //       items={[
+          //         {
+          //           type: "info",
+          //           dismissible: true,
+          //           content: "This is an info flash message.",
+          //           id: "message_1",
+          //         },
+          //       ]}
+          //     />
+          //   </Box>
+          // }
           toolsHide
           content={
             <SpaceBetween size="xs">
