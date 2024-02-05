5bb3a94b7a448bbedd273a509f3bf617f98e374e
sanjaykumbhar26 <webvedkumbhar@gmail.com>
2024-02-05 18:48:44 +0530

    update table and header task

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import {
   Header,
-  Container,
   ContentLayout,
-  Icon,
   Box,
   AppLayout,
   BreadcrumbGroup,
-  Flashbar,
-  HelpPanel,
   Link,
   SideNavigation,
   SplitPanel,
@@ -17,10 +13,13 @@ import {
 import { I18nProvider } from "@cloudscape-design/components/i18n";
 import messages from "@cloudscape-design/components/i18n/messages/all.en";
 import WebViewer from "@pdftron/webviewer";
-import ButtonDropdown from "@cloudscape-design/components/button-dropdown";
 import { applyTheme } from '@cloudscape-design/components/theming';
 import TopNavigation from "@cloudscape-design/components/top-navigation";
-
+import Table from "@cloudscape-design/components/table";
+import Button from "@cloudscape-design/components/button";
+import TextFilter from "@cloudscape-design/components/text-filter";
+import Pagination from "@cloudscape-design/components/pagination";
+import CollectionPreferences from "@cloudscape-design/components/collection-preferences";
 
 const theme = {
    tokens: {
@@ -33,18 +32,16 @@ const theme = {
 
 applyTheme({ theme });
 
-const headerStyle = {
-  padding: 0,
-};
-
 const appStyle = {
   backgroundColor: '#000',
 };
 
+
+
 // Assign your function to a variable
 export function Home() {
   const viewer = useRef(null);
-  const [navOpen, setNavOpen] = useState(false);
+  const [, ] = useState(false);
 
   useEffect(() => {
     if (viewer.current != null)
@@ -61,9 +58,13 @@ export function Home() {
       });
   });
   const LOCALE = "en";
-
+   const [
+    selectedItems,
+    setSelectedItems
+  ] = React.useState([{ name: "Item 2" }]);
   return (
     <I18nProvider locale={LOCALE} messages={[messages]}>
+      <div  style={{ position: 'sticky', top: 0, zIndex: 1002 }}>
       <TopNavigation
         id="topNavigation"
       identity={{
@@ -142,44 +143,11 @@ export function Home() {
           ]
         }
       ]}
-    />
+        />
+        </div>
       <ContentLayout
         disableOverlap
         disableContentPaddings
-        // header={
-        //   <Header
-        //     className="header"
-        //     disableOverlap
-        //     disableContentPaddings
-        //     actions={
-        //       <ButtonDropdown
-        //           items={[
-        //             { text: "Delete", id: "rm", disabled: false },
-        //             { text: "Move", id: "mv", disabled: false },
-        //             { text: "Rename", id: "rn", disabled: true },
-        //             {
-        //               id: "view",
-        //               text: "View metrics",
-        //               href: "https://example.com",
-        //               external: true,
-        //               externalIconAriaLabel: "(opens in new tab)",
-        //             },
-        //           ]}
-        //           className="buttonDropdown"
-        //         >
-        //           Short
-        //         </ButtonDropdown>
-        //     }
-        //     style={headerStyle}
-        //   >
-        //         <img
-        //           src="/RD_Horizozntal_White_Logo@2x.png"
-        //           alt="placeholder"
-        //           width="130px"
-        //         />
-        //         Adobe Acrobat Sign
-        //   </Header>
-        // }
       >
         <AppLayout
           contentType="default"
@@ -187,10 +155,6 @@ export function Home() {
           style={appStyle}
           headerSelector="#topNavigation"
           className="appLayout"
-          // navigationOpen={navOpen}
-          // onNavigationChange={(changeDetails) => {
-          //   setNavOpen(changeDetails.detail.open);
-          // }}
           navigation={
             <SideNavigation
               header={{
@@ -200,20 +164,6 @@ export function Home() {
               items={[{ type: "link", text: `Paage #1`, href: `#` }]}
             />
           }
-          // notifications={
-          //   <Box padding={{ top: "s" }}>
-          //     <Flashbar
-          //       items={[
-          //         {
-          //           type: "info",
-          //           dismissible: true,
-          //           content: "This is an info flash message.",
-          //           id: "message_1",
-          //         },
-          //       ]}
-          //     />
-          //   </Box>
-          // }
           toolsHide
           content={
             <SpaceBetween size="xs">
@@ -223,15 +173,279 @@ export function Home() {
                   { text: "Service", href: "#" },
                 ]}
               />
-              <Container
-                header={
-                  <Header variant="h2" description="Container description">
-                    Container header
-                  </Header>
-                }
-              >
-                <div className="contentPlaceholder" />
-              </Container>
+            
+                  <Table
+      onSelectionChange={({ detail }) =>
+        setSelectedItems(detail.selectedItems)
+      }
+      selectedItems={selectedItems}
+      ariaLabels={{
+        selectionGroupLabel: "Items selection",
+        allItemsSelectionLabel: ({ selectedItems }) =>
+          `${selectedItems.length} ${
+            selectedItems.length === 1 ? "item" : "items"
+          } selected`,
+        itemSelectionLabel: ({ selectedItems }, item) =>
+          item.name
+      }}
+      columnDefinitions={[
+        {
+          id: "variable",
+          header: "Variable name",
+          cell: item => <Link href="#">{item.name}</Link>,
+          sortingField: "name",
+          isRowHeader: true
+        },
+        {
+          id: "value",
+          header: "Text value",
+          cell: item => item.alt,
+          sortingField: "alt"
+        },
+        {
+          id: "type",
+          header: "Type",
+          cell: item => item.type
+        },
+        {
+          id: "description",
+          header: "Description",
+          cell: item => item.description
+        }
+      ]}
+      columnDisplay={[
+        { id: "variable", visible: true },
+        { id: "value", visible: true },
+        { id: "type", visible: true },
+        { id: "description", visible: true }
+      ]}
+      items={[
+        {
+          name: "Item 1",
+          alt: "First",
+          description: "This is the first item",
+          type: "1A",
+          size: "Small"
+        },
+        {
+          name: "Item 2",
+          alt: "Second",
+          description: "This is the second item",
+          type: "1B",
+          size: "Large"
+        },
+        {
+          name: "Item 3",
+          alt: "Third",
+          description: "-",
+          type: "1A",
+          size: "Large"
+        },
+        {
+          name: "Item 4",
+          alt: "Fourth",
+          description: "This is the fourth item",
+          type: "2A",
+          size: "Small"
+        },
+        {
+          name: "Item 5",
+          alt: "-",
+          description:
+            "This is the fifth item with a longer description",
+          type: "2A",
+          size: "Large"
+        },
+        {
+          name: "Item 6",
+          alt: "Sixth",
+          description: "This is the sixth item",
+          type: "1A",
+          size: "Small"
+        },
+         {
+          name: "Item 7",
+          alt: "First",
+          description: "This is the first item",
+          type: "1A",
+          size: "Small"
+        },
+        {
+          name: "Item 8",
+          alt: "Second",
+          description: "This is the second item",
+          type: "1B",
+          size: "Large"
+        },
+        {
+          name: "Item 9",
+          alt: "Third",
+          description: "-",
+          type: "1A",
+          size: "Large"
+        },
+        {
+          name: "Item 10",
+          alt: "Fourth",
+          description: "This is the fourth item",
+          type: "2A",
+          size: "Small"
+        },
+        {
+          name: "Item 11",
+          alt: "-",
+          description:
+            "This is the fifth item with a longer description",
+          type: "2A",
+          size: "Large"
+        },
+        {
+          name: "Item 12",
+          alt: "Sixth",
+          description: "This is the sixth item",
+          type: "1A",
+          size: "Small"
+        },
+         {
+          name: "Item 13",
+          alt: "First",
+          description: "This is the first item",
+          type: "1A",
+          size: "Small"
+        },
+        {
+          name: "Item 14",
+          alt: "Second",
+          description: "This is the second item",
+          type: "1B",
+          size: "Large"
+        },
+        {
+          name: "Item 15",
+          alt: "Third",
+          description: "-",
+          type: "1A",
+          size: "Large"
+        },
+        {
+          name: "Item 16",
+          alt: "Fourth",
+          description: "This is the fourth item",
+          type: "2A",
+          size: "Small"
+        },
+        {
+          name: "Item 17",
+          alt: "-",
+          description:
+            "This is the fifth item with a longer description",
+          type: "2A",
+          size: "Large"
+        },
+        {
+          name: "Item 18",
+          alt: "Sixth",
+          description: "This is the sixth item",
+          type: "1A",
+          size: "Small"
+        }
+      ]}
+      loadingText="Loading resources"
+      selectionType="multi"
+      trackBy="name"
+      empty={
+        <Box
+          margin={{ vertical: "xs" }}
+          textAlign="center"
+          color="inherit"
+        >
+          <SpaceBetween size="m">
+            <b>No resources</b>
+            <Button>Create resource</Button>
+          </SpaceBetween>
+        </Box>
+      }
+      filter={
+        <TextFilter
+          filteringPlaceholder="Find resources"
+          filteringText=""
+        />
+      }
+      header={
+        <Header
+          counter={
+            selectedItems.length
+              ? "(" + selectedItems.length + "/10)"
+              : "(10)"
+          }
+        >
+          Table with common features
+        </Header>
+      }
+      pagination={
+        <Pagination currentPageIndex={1} pagesCount={2} />
+      }
+      preferences={
+        <CollectionPreferences
+          title="Preferences"
+          confirmLabel="Confirm"
+          cancelLabel="Cancel"
+          preferences={{
+            pageSize: 10,
+            contentDisplay: [
+              { id: "variable", visible: true },
+              { id: "value", visible: true },
+              { id: "type", visible: true },
+              { id: "description", visible: true }
+            ]
+          }}
+          pageSizePreference={{
+            title: "Page size",
+            options: [
+              { value: 10, label: "10 resources" },
+              { value: 20, label: "20 resources" }
+            ]
+          }}
+          wrapLinesPreference={{}}
+          stripedRowsPreference={{}}
+          contentDensityPreference={{}}
+          contentDisplayPreference={{
+            options: [
+              {
+                id: "variable",
+                label: "Variable name",
+                alwaysVisible: true
+              },
+              { id: "value", label: "Text value" },
+              { id: "type", label: "Type" },
+              { id: "description", label: "Description" }
+            ]
+          }}
+          stickyColumnsPreference={{
+            firstColumns: {
+              title: "Stick first column(s)",
+              description:
+                "Keep the first column(s) visible while horizontally scrolling the table content.",
+              options: [
+                { label: "None", value: 0 },
+                { label: "First column", value: 1 },
+                { label: "First two columns", value: 2 }
+              ]
+            },
+            lastColumns: {
+              title: "Stick last column",
+              description:
+                "Keep the last column visible while horizontally scrolling the table content.",
+              options: [
+                { label: "None", value: 0 },
+                { label: "Last column", value: 1 }
+              ]
+            }
+          }}
+        />
+      }
+    />
+          
             </SpaceBetween>
           }
           splitPanel={
@@ -291,4 +505,5 @@ const disabledElements = [
   //Thumbnail
   "thumbRotateCounterClockwise",
   "thumbRotateClockwise",
+  
 ];
